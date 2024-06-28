90f44300d90e73d15ddf809914309469b821a824
kimdh42 <kimdh1992@gmail.com>
2024-06-28 17:06:30 +0900

    #57 (06/28) employee

--- src/apis/EmployeeAPICalls.js
+++ src/apis/EmployeeAPICalls.js
@@ -1,5 +1,5 @@
 import { request } from './api';
-import { getDepartments, getDeptEmployees, getMyInfo, getRecordcard } from '../modules/EmployeeModules';
+import { getDepartments, getDeptEmployees, getMyInfo, getRecordcard, getDeptDetail, success, getTeamRecordcard, registEmployees } from '../modules/EmployeeModules';
 
 export const callDepartmentEmployeesAPI = () => {
 
@@ -88,11 +88,11 @@ export const callDepartmentsAPI = () => {
         try {
             const result = await request('GET', '/employee/departments');
 
-            console.log('callDepartmentsAPI result: ', result);
+            // console.log('callDepartmentsAPI result: ', result);
 
             if(result && result.status === 200) {
 
-                console.log('Dispatching getDepartments with data: ', result.data);
+                // console.log('Dispatching getDepartments with data: ', result.data);
 
                 dispatch(getDepartments(result.data));
 
@@ -102,6 +102,245 @@ export const callDepartmentsAPI = () => {
             }
         } catch(error) {
             console.error('부서목록 조회 실패(error): ', error);
+        }
+    };
+};
+
+export const callDeptDetailAPI = (deptCode) => { 
+
+    return async ( dispatch, getState) => {
+
+        try {
+            const result = await request('GET', `/employee/deptDetailList/${deptCode}`);
+
+            console.log('callDeptDetailAPI result: ', result);
+
+            if(result && result.status === 200) {
+
+                console.log('Dispatching getDeptDetail with data: ', result.data);
+
+                dispatch(getDeptDetail(result.data));
+
+            } else {
+
+                console.log('부서상세 조회 실패(result): ', result);
+            }
+        } catch(error) {
+            console.error('부서상세 조회 실패(error): ', error);
+        }
+    };
+};
+
+export const callRegistDeptAPI = (deptTitle) => {
+
+    return async (dispatch, getState) => {
+        try{
+            const accessToken = localStorage.getItem('access-token');
+            const headers = {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            };
+
+            const requestData = {
+                dept_title: deptTitle // 부서 제목을 요청 데이터에 추가
+            };
+
+            const result = await request('POST', `/employee/registDept`, headers, requestData);
+            
+            console.log('callRegistDeptAPI : ', result);
+    
+            if(result && result.status === 201) {
+    
+                dispatch(success());
+                dispatch(callDepartmentsAPI()); // 부서 등록 성공 후에 부서 목록 다시 가져오기
+            }
+        } catch (error) {
+            console.error('부서 등록 실패 : ', error)
+        }
+    };
+};
+
+export const callRegistDeptRelationsAPI = (parentDeptCode, subDeptCode) => {
+
+    return async (dispatch, getState) => {
+        try {
+            const accessToken = localStorage.getItem('access-token');
+            const headers = {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            };
+
+            const requestData = {
+                parentDepartment: { dept_code: parentDeptCode },
+                subDepartment: { dept_code: subDeptCode }
+            };
+
+            const result = await request('POST', `/employee/registDeptRelations`, headers, requestData);
+
+            console.log('callRegistDeptRelationsAPI : ', result);
+
+            if(result && result.status === 201) {
+
+                dispatch(success());
+                dispatch(callDepartmentsAPI());
+            }
+        } catch (error) {
+            console.error('부서관계 등록 실패 : ', error)
+        }
+    };
+};
+
+export const callDeleteDeptRelationsAPI = (parentDeptCode, subDeptCode) => {
+
+    return async (dispatch, getState) => {
+        try {
+            const accessToken = localStorage.getItem('access-token');
+            const headers = {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            };
+
+            const requestData = {
+                parentDepartment: { dept_code: parentDeptCode },
+                subDepartment: { dept_code: subDeptCode }
+            };
+
+            const result = await request('DELETE', `/employee/deleteDeptRelations`, headers, requestData);
+
+            console.log('callDeleteDeptRelationsAPI : ', result);
+
+            if (result && result.status === 204) {
+                
+                dispatch(success());
+                dispatch(callDepartmentsAPI());
+            }
+        } catch(error) {
+            console.error('부서관계 삭제 실패', error)
+        }
+    };
+};
+
+export const callModifyDeptRelationsAPI = (parentDeptCode, subDeptCode, deptRelationsCode) => {
+
+    return async (dispatch, getState) => {
+        try {
+            const accessToken = localStorage.getItem('access-token');
+            const headers = {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            };
+
+            const requestData = {
+                parentDepartment: { dept_code: parentDeptCode },
+                subDepartment: { dept_code: subDeptCode }
+            };
+
+            const result = await request('PUT', `/employee/modifyDeptRelations/${deptRelationsCode}`, headers, requestData);
+
+            console.log('callModifyDeptRelationsAPI : ', result);
+
+            if(result && result.status === 201) {
+
+                dispatch(success(result.data));
+                dispatch(callDepartmentsAPI());
+                
+            }
+  
+        } catch(error) {
+            console.error('부서관계 수정 실패 : ', error)
+        }
+    };
+};
+
+export const callResetPasswordAPI = (empCode) => {
+
+    return async (dispatch, getState) => {
+        try {
+            const accessToken = localStorage.getItem('access-token');
+            const headers = {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            };
+
+            const result = await request('PUT', `/employee/resetEmpPass/${empCode}`, headers);
+
+            console.log('callResetPasswordAPI : ', result);
+
+            if(result && result.status === 201) {
+
+                console.log('Dispatching successResetPass with data: ', result.data);
+
+                dispatch(success(result.data));
+                // dispatch(callDepartmentEmployeesAPI());
+            } else {
+                console.log('비밀번호 초기화 실패(result)', result);
+            }
+        } catch (error) {
+            console.error('비밀번호 초기화 실패(error) ', error);
+        }
+    };
+};
+
+export const callTeamRecordCardAPI = (empCode) => {
+
+    return async (dispatch, getState) => {
+        try {
+            
+            const accessToken = localStorage.getItem('access-token');
+            
+            const headers = {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            };
+        
+            const result = await request('GET', `/employee/teamRecordCard/${empCode}`, headers);
+
+            console.log('callTeamRecordCardAPI : ', result);
+
+            if(result && result.status === 200) {
+
+                console.log('Dispatching getRecordcard with data: ', result);
+
+                dispatch(getTeamRecordcard(result.data));
+
+            } else {
+                console.log('팀원 인사기록카드 조회 실패(result)', result)
+            }
+        } catch (error) {
+            console.error('팀원 인사기록카드 조회 실패(eroor)', error)
+        }      
+    };
+};
+
+export const callRegistEmployeesAPI = (employeesData) => {
+    return async (dispatch, getState) => {
+        try {
+            const accessToken = localStorage.getItem('access-token');
+            const headers = {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            };
+
+            console.log('Sending Payload employeesData:', employeesData);
+
+            const result = await request('POST', '/employee/empsRegist', headers, employeesData); 
+
+            console.log('callRegistEmployeeAPI result : ', result);
+
+            if (result && result.status === 201) {
+                console.log('사원 등록 성공:', result.data);
+                dispatch(registEmployees(result.data)); // 등록 성공 시 리듀서에 액션 디스패치
+                alert('사원이 성공적으로 등록되었습니다.');
+            } else {
+                console.log('사원 등록 실패:', result);
+                alert('사원 등록 중 오류가 발생하였습니다. result');
+            }
+        } catch (error) {
+            console.error('사원 등록 실패:', error);
+            if (error.response) {
+                console.error('Server Response:', error.response.data);
+            }
+            alert('사원 등록 중 오류가 발생하였습니다. error');
         }
     };
 };
--- src/modules/EmployeeModules.js
+++ src/modules/EmployeeModules.js
@@ -6,43 +6,78 @@ const initialState = {
     employee: {},
     recordCard: {},
     departments: [],
+    deptDetail: {},
+    success: {},
+    teamRecordCard: {},
+    registEmployees: [],
 };
 
 /* 액션 타입 */
-const GET_DEPT_EMPLOYEES = 'employee/GET_DEPT_EMPLOYEES';
-const GET_MY_INFO = 'employee/GET_MY_INFO';
-const GET_RECORDCARD = 'employee/GET_RECORDCARD';
-const GET_DEPARTMENS = 'employee/GET_DEPARTMENS'
+const GET_DEPT_EMPLOYEES = 'employee/GET_DEPT_EMPLOYEES';   // 부서인원
+const GET_MY_INFO = 'employee/GET_MY_INFO';                 // 내정보 조회
+const GET_RECORDCARD = 'employee/GET_RECORDCARD';           // 나의 인사기록카드
+const GET_DEPARTMENTS = 'employee/GET_DEPARTMENTS';         // 부서 목록
+const GET_DEPT_DETAIL = 'employee/GET_DEPT_DETAIL';         // 부서 상세정보
+const SUCCESS = 'employee/SUCCESS';                         // 등록, 수정, 삭제
+const GET_TEAM_RECORDCARD = 'employee/GET_TEAM_RECORDCARD'; // 팀원 인사기록카드
+const REGIST_EMPLOYEES = 'employee/REGIST_EMPLOYEES';
+
 
 /* 액션 함수 */
-export const { employee : { getDeptEmployees, getMyInfo, getRecordcard, getDepartments } } = createActions ({
+export const { employee : { getDeptEmployees, getMyInfo, getRecordcard, getDepartments, getDeptDetail, success, getTeamRecordcard, registEmployees } } = createActions ({
     [GET_DEPT_EMPLOYEES] : result => {
 
-        console.log('Action payload:', result);
+        console.log('GET_DEPT_EMPLOYEES Action payload:', result);
 
         return {employees: result};
 
     },
     [GET_MY_INFO] : result => {
 
-        console.log('Action payload', result);
+        console.log('GET_MY_INFO Action payload', result);
 
         return {employee: result};
     },
     [GET_RECORDCARD] : result => {
 
-        console.log('Action payload : ', result);
+        console.log('GET_RECORDCARD Action payload : ', result);
 
         return {recordCard: result};
     },
-    [GET_DEPARTMENS] : result => {
+    [GET_DEPARTMENTS] : result => {
 
-        console.log('Action payload:', result);
+        // console.log('GET_DEPARTMENTS Action payload:', result);
 
         return {departments: result};
 
     },
+    [GET_DEPT_DETAIL] : result => {
+
+        console.log('GET_DEPT_DETAIL Action payload', result);
+
+        return {deptDetail : result};
+    },
+    [SUCCESS] : result => {
+
+        console.log('SUCCESS Action payload', result);
+
+        return {success : true};
+    },
+    [GET_TEAM_RECORDCARD] : result => {
+
+        console.log('GET_TEAM_RECORDCARD Action payload', result);
+
+        return {teamRecordCard : result};
+    },
+    [REGIST_EMPLOYEES] : result => {
+
+        console.log('REGIST_EMPLOYEES Action payload : ', result);
+
+        return { registEmployees : result};
+    },
+
 });
+
 
 /* 리듀서 함수 */
 const employeeReducer = handleActions({
@@ -75,13 +110,49 @@ const employeeReducer = handleActions({
             recordCard: payload.recordCard,
         };
     },
-    [GET_DEPARTMENS] : ( state, { payload }) => {
+    [GET_DEPARTMENTS] : ( state, { payload }) => {
 
-        console.log('Reducer GET_DEPARTMENS payload:', payload);
+        // console.log('Reducer GET_DEPARTMENS payload:', payload);
 
         return {
             ...state,
             departments: payload.departments,
+        };
+    },
+    [GET_DEPT_DETAIL] : ( state, { payload }) => {
+
+        console.log('Reducer GET_DEPT_DETAIL payload : ', payload);
+
+        return {
+            ...state,
+            deptDetail: payload.deptDetail,
+        };
+    },
+    [SUCCESS] : ( state, { payload }) => {
+
+        console.log('Reducer SUCCESS payload : ', payload);
+
+        return {
+            ...state,
+            success: payload.success,
+        };
+    },
+    [GET_TEAM_RECORDCARD] : ( state, { payload }) => {
+
+        console.log('Reducer GET_TEAM_RECORDCARD payload : ', payload);
+
+        return {
+            ...state,
+            teamRecordCard: payload.teamRecordCard,
+        };
+    },
+    [REGIST_EMPLOYEES] : ( state, { payload }) => {
+
+        console.log('Reducer REGIST_EMPLOYEES payload : ', payload);
+
+        return {
+            ...state,
+            registEmployees: payload.registEmployees,
         };
     },
 

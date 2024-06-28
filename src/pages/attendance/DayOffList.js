f457312db30a200343251706fb509facd3755d6d
SILVER-BE <dhfldmlwkrdmsdusaht@gmail.com>
2024-06-28 18:19:27 +0900

    #62 [메인/근태] 기본 화면 구성

--- src/pages/attendance/DayOffList.js
+++ src/pages/attendance/DayOffList.js
@@ -1,11 +1,178 @@
-import { Outlet } from "react-router-dom";
-import AttendanceNav from "../../components/commons/AttendanceNav";
+import React, { useEffect, useState } from 'react';
+import AttendanceSummary from "./component/AttendanceSummary";
+import {useDispatch, useSelector} from "react-redux";
+import DefaultSchedule from "./component/DefaultSchedule";
+import {callAttendanceTodayAPI, callMyAttendanceForWeekAPI, callMyInfoAPI} from "../../apis/AttendancelAPICalls";
+import WorkStatus from "./component/WorkStatus";
 
-function DayOffList(){
-    return(
+function DayOffList() {
+    const dispatch = useDispatch();
+    const attendancesToday = useSelector((state) => state.attendanceReducer.attendanceToday);
+    const employee = useSelector((state) => state.attendanceReducer.employee);
+
+    useEffect(() => {
+        dispatch(callMyInfoAPI());
+        dispatch(callMyAttendanceForWeekAPI());
+        dispatch(callAttendanceTodayAPI());
+    }, [dispatch]);
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        // Handle search logic here
+        console.log('Search button clicked');
+    };
+
+    return (
         <>
-            상세 휴가 페이지
+            <div className="ly_cont ly_flex">
+                <div className="hp_mr50">
+                    <div style={{ position: "sticky" }}>
+                        <AttendanceSummary attendancesToday={attendancesToday} />
+                        <DefaultSchedule employee={employee}/>
+                        <WorkStatus/>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="" style={{ width: '900px' }}>
+                        <div className="ly_spaceBetween ly_fitemEnd hp_mb30">
+                            <h4 className="el_lv1Head" style={{ paddingLeft: '10px' }}>상세 휴가 현황</h4>
+                        </div>
+                        <div className="ly_spaceBetween hp_mt20"
+                             style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <select className="hp_mr5" defaultValue="">
+                                    <option value="">2024년</option>
+                                </select>
+                                <select className="hp_mr5" defaultValue="">
+                                    <option value="">6월</option>
+                                </select>
+                            </div>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <select className="hp_mr5" defaultValue="">
+                                    <option value="">상위부서명</option>
+                                </select>
+                                <select className="hp_mr5" defaultValue="">
+                                    <option value="">하위부서명</option>
+                                </select>
+                                <select className="hp_mr10" defaultValue="">
+                                    <option value="">세부팀명</option>
+                                </select>
+                                <form onSubmit={handleSearch} style={{ display: 'flex', alignItems: 'center' }}>
+                                    <input type="text" className="hp_mr5" id="" name="" defaultValue=""
+                                           placeholder="검색어를 입력해주세요"/>
+                                    <button type="submit" className="el_btnS el_btnblueBord" id="" name="">
+                                        검색
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                        <section className="bl_sect hp_mt10" style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+                            <table className="bl_tb1" style={{ width: '100%' }}>
+                                <colgroup>
+                                    <col style={{ width: "50px" }}/>
+                                    <col style={{ width: "120px" }}/>
+                                    <col style={{ width: "120px" }}/>
+                                    <col style={{ width: "120px" }}/>
+                                    <col style={{ width: "120px" }}/>
+                                    <col style={{ width: "120px" }}/>
+                                </colgroup>
+                                <thead>
+                                <tr>
+                                    <th scope="col"><input type="checkbox" className="" id="" name=""
+                                                           value="checkAll"/></th>
+                                    <th scope="col">발령일</th>
+                                    <th scope="col">이름</th>
+                                    <th scope="col">발령종류</th>
+                                    <th scope="col">발령 전</th>
+                                    <th scope="col">발령 후</th>
+                                </tr>
+                                </thead>
+                                {/* <!-- 5개씩 나열 --> */}
+                                <tbody>
+                                <tr>
+                                    <th scope="row"><input type="checkbox" className="" id="" name=""
+                                                           value="checkOne"/></th>
+                                    <td>2024-12-34</td>
+                                    <td className="" style={{ textAlign: 'center' }}>홍길동</td>
+                                    <td>이동</td>
+                                    <td>영업팀</td>
+                                    <td>고객관리팀</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row"><input type="checkbox" className="" id="" name=""
+                                                           value="checkOne"/></th>
+                                    <td>2024-12-34</td>
+                                    <td className="" style={{ textAlign: 'center' }}>홍길동</td>
+                                    <td>이동</td>
+                                    <td>영업팀</td>
+                                    <td>고객관리팀</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row"><input type="checkbox" className="" id="" name=""
+                                                           value="checkOne"/></th>
+                                    <td>2024-12-34</td>
+                                    <td className="" style={{ textAlign: 'center' }}>홍길동</td>
+                                    <td>이동</td>
+                                    <td>영업팀</td>
+                                    <td>고객관리팀</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row"><input type="checkbox" className="" id="" name=""
+                                                           value="checkOne"/></th>
+                                    <td>2024-12-34</td>
+                                    <td className="" style={{ textAlign: 'center' }}>홍길동</td>
+                                    <td>이동</td>
+                                    <td>영업팀</td>
+                                    <td>고객관리팀</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row"><input type="checkbox" className="" id="" name=""
+                                                           value="checkOne"/></th>
+                                    <td>2024-12-34</td>
+                                    <td className="" style={{ textAlign: 'center' }}>홍길동</td>
+                                    <td>이동</td>
+                                    <td>영업팀</td>
+                                    <td>고객관리팀</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </section>
+                        <div className="ly_spaceBetween ly_fitemC hp_mt10">
+                            <div className="hp_ml10 hp_7Color">총 1 / <b className="hp_0Color hp_fw700">1</b> 페이지
+                            </div>
+                        </div>
+                        <section className="bl_sect hp_mt10 hp_padding5" style={{ textAlign: 'center' }}>
+                            <div className="bl_paging">
+                                <a className="bl_paging__btn bl_paging__first" href="" title="첫 페이지로 이동"></a>
+                                <a className="bl_paging__btn bl_paging__prev" href="" title="이전 페이지로 이동"></a>
+                                <a className="bl_paging__btn bl_paging__num" href="">1</a>
+                                <a className="bl_paging__btn bl_paging__next" href="" title="다음 페이지로 이동"></a>
+                                <a className="bl_paging__btn bl_paging__last" href="" title="마지막 페이지로 이동"></a>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+                <div className="" style={{ paddingLeft: '60px' }}>
+                    <section className="bl_sect hp_padding20 el_shadowD4 hp_mb30 section3">
+                        <div>
+                            <p className="hp_fw700 section3_title">빠른 메뉴</p>
+                        </div>
+                        <div>
+                            <button type="button" className="el_btn0Back el_btnF hp_mt10 section3_btn">
+                                출장 신청
+                            </button>
+                            <button type="button" className="el_btn0Back el_btnF hp_mt10 section3_btn">
+                                초과 신청
+                            </button>
+                            <button type="button" className="el_btn0Back el_btnF hp_mt10 section3_btn">
+                                휴가 신청
+                            </button>
+                        </div>
+                    </section>
+                </div>
+            </div>
         </>
-    )
+    );
 }
+
 export default DayOffList;
--- src/pages/attendance/Preferences.js
+++ src/pages/attendance/Preferences.js
@@ -1,11 +1,76 @@
-import { Outlet } from "react-router-dom";
-import AttendanceNav from "../../components/commons/AttendanceNav";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { callDepartmentsAPI } from '../../apis/EmployeeAPICalls';
+import DepartmentTree from '../employee/DepartmentTree';
+
 
 function Preferences(){
+
+    const dispatch = useDispatch();
+    const departments = useSelector(state => state.employeeReducer.employee.departments);
+
+    useEffect(() => {
+        dispatch(callDepartmentsAPI());
+    }, [dispatch]);
+
     return(
-        <>
-            환경설정 페이지
-        </>
+        <div className="ly_cont">
+            <h4 className="el_lv1Head hp_mb30 hp_ml20">근무시간 관리</h4>
+            <section className="bl_sect ly_flex" style={{height: "calc(100% - 30px - 42px)"}} >
+                <div className="ly_body hp_bordDE hp_w30 hp_margin15 ly_flexC hp_f9Back ly_fdirecCol ly_fitemC" style={{height: "calc(100% - 20px)"}}>
+                    <div className="hp_w90 hp_mt30" style={{height: "20%", textAlign: 'center' }}>
+                        <select className="hp_w100">
+                            <option value="">선택</option>
+                        </select>
+                        <div className="hp_w100 ly_flex" style={{height: "30px"}}>
+                           <input type="text" className="hp_mt15 hp_floatL" style={{width: "100%"}} id="" name="" value="" placeholder="검색" />
+                           <input type="submit" className="el_btnS el_btnblueBord hp_ml5 hp_mt15 hp_floatR" id="" name="" value="검색" />
+                        </div>
+                    </div>
+                    <div className="hp_w90 hp_mt10 hp_mb30 hp_bordDE hp_br5 hp_fBack" style={{ textAlign: 'left', height: "100%" }}>
+                        {/* <DepartmentTree departments={departments} /> */}
+                    </div>
+                </div>
+                <div className="ly_body hp_bordDE hp_w70 hp_margin15 hp_f9Back ly_flexC hp_f9Back ly_fdirecCol ly_fitemC" style={{height: "calc(100% - 20px)"}}>
+                    <div className="hp_w95 hp_h100 hp_bordDE hp_br5 hp_fBack hp_mt30 hp_mb30" style={{ textAlign: 'center' }}>
+                        <h5 className="hp_fw700 hp_fs18 hp_mt15 hp_mb30 hp_floatL hp_ml5 hp_ml20">지정 출/퇴근시간 정보</h5>
+
+                        <table className="bl_tb3">
+                            <colgroup>
+                                <col style={{width: "120px"}} />
+                                <col style={{width: "*"}} />
+                            </colgroup>
+                            <tbody>
+                            <tr>
+                                <th scope="row">근무코드</th>
+                                <td><input type="text" className="hp_w100"/></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">상위부서</th>
+                                <td><input type="text" className="hp_w100"/></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">하위부서</th>
+                                <td><input type="text" className="hp_w100"/></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">팀명</th>
+                                <td><input type="text" className="hp_w100"/></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">사원명</th>
+                                <td><input type="text" className="hp_w100"/></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">생성일</th>
+                                <td><input type="text" className="hp_w100"/></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
 export default Preferences;
